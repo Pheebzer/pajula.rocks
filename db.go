74a8d91dface0673d2b4348a772e27a39fb4427a
Pete <pheebzer@gmail.com>
2024-10-19 16:00:48 +0300

    wrap statements and transaction, update songs based on snapshot_id, only delete old songs

--- db.go
+++ db.go
@@ -8,22 +8,23 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
-var insertTrackQuery string = `
-INSERT INTO tracks (id,name,album,artist,user,added_at,duration_ms,snapshot_id)
-  VALUES (?,?,?,?,?,?,?,?);
-`
-var deleteTracksQuery = `
-DELETE FROM tracks;
-`
-var getSnapshotQuery = `
-SELECT snapshot_id FROM metadata;
-`
-var updateSnapshotQuery = `
-UPDATE metadata SET snapshot_id=?;
-`
+type ImporterTx struct {
+	tx         *sql.Tx
+	statements Statements
+}
+
+type Statements struct {
+	InsertUpdateTrack *sql.Stmt
+	DeleteTracks      *sql.Stmt
+	GetSnapshotId     *sql.Stmt
+	UpdateSnapshotId  *sql.Stmt
+}
 
 func InitDB(cfg *Config) (*sql.DB, error) {
-	dsn := fmt.Sprintf("%s:%s@/%s", cfg.Db.User, cfg.Db.Password, cfg.Db.Database)
+	dsn := fmt.Sprintf("%s:%s@/%s",
+		cfg.Db.User,
+		cfg.Db.Password,
+		cfg.Db.Database)
 	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		return nil, err
@@ -33,4 +34,39 @@ func InitDB(cfg *Config) (*sql.DB, error) {
 	db.SetMaxIdleConns(10)
 
 	return db, nil
+}
+
+func StartTransaction(db *sql.DB) (*ImporterTx, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return nil, err
+	}
+	statements := getStatements(tx)
+	return &ImporterTx{
+		tx:         tx,
+		statements: statements,
+	}, nil
+}
+
+func prepareStmt(tx *sql.Tx, query string) *sql.Stmt {
+	stmt, err := tx.Prepare(query)
+	if err != nil {
+		panic(err)
+	}
+	return stmt
+}
+
+func getStatements(tx *sql.Tx) Statements {
+	return Statements{
+		InsertUpdateTrack: prepareStmt(tx, `
+			INSERT INTO tracks (id,name,album,artist,user,added_at,duration_ms,snapshot_id)
+			VALUES (?,?,?,?,?,?,?,?)
+			ON DUPLICATE KEY UPDATE snapshot_id = VALUES(snapshot_id);`),
+		DeleteTracks: prepareStmt(tx, `
+			DELETE FROM tracks WHERE snapshot_id = ?;`),
+		GetSnapshotId: prepareStmt(tx, `
+			SELECT snapshot_id FROM metadata;`),
+		UpdateSnapshotId: prepareStmt(tx, `
+			UPDATE metadata SET snapshot_id = ?;`),
+	}
 }
--- main.go
+++ main.go
@@ -1,10 +1,11 @@
-package main
+package main //32twOqGf8gIswTgzG3IKxP
 
 import (
 	"database/sql"
 	"fmt"
 	"net/http"
 	"os"
+	"runtime/pprof"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -20,20 +21,32 @@ func rollBackTx(tx *sql.Tx) {
 }
 
 func main() {
+
+	// Create a CPU profile file.
+	f, err := os.Create("beans.prof")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// Start CPU profiling.
+	if err := pprof.StartCPUProfile(f); err != nil {
+		panic(err)
+	}
+	defer pprof.StopCPUProfile()
+
 	cfp := os.Getenv("CONFIG_FILE")
 	if cfp == "" {
 		cfp = "config.yaml"
 	}
 	cfg := parseConfig(cfp)
 
-	// initialise database and start new transaction
 	db, err := InitDB(cfg)
 	if err != nil {
 		panic(err)
 	}
 	defer db.Close()
 
-	// initialise http client and related data
 	c := &http.Client{}
 	token := fetchAccessToken(c, cfg.Spotify.TokenEndpoint, cfg.Spotify.ApiKey)
 	filterStr := "fields=next,items(added_by.id,added_at,track(name,id,duration_ms,album(name),artists(name))"
@@ -41,63 +54,36 @@ func main() {
 	snapshotUrl := fmt.Sprintf("%s/%s?fields=snapshot_id", cfg.Spotify.PlaylistEndpoint, cfg.Spotify.PlaylistId)
 	nextPage := fmt.Sprintf("%s?%s&limit=100", pageBaseUrl, filterStr)
 
-	// start new transaction, prepare statements
-	tx, err := db.Begin()
-	if err != nil {
-		panic(err)
-	}
-	getSnapshotId, err := tx.Prepare(getSnapshotQuery)
-	if err != nil {
-		panic(err)
-	}
-	updateSnapshotId, err := tx.Prepare(updateSnapshotQuery)
-	if err != nil {
-		panic(err)
-	}
-	insertTrack, err := tx.Prepare(insertTrackQuery)
-	if err != nil {
-		panic(err)
-	}
-	deleteTracks, err := tx.Prepare(deleteTracksQuery)
+	itx, err := StartTransaction(db)
 	if err != nil {
 		panic(err)
 	}
 
 	// check if the playlist has changed, return early if there is nothing to import
 	newSnapshotId := fetchSnapshotId(c, snapshotUrl, token)
-	var snapshotId string
-	if err := getSnapshotId.QueryRow().Scan(&snapshotId); err != nil {
+	var oldSnapshotId string
+	if err := itx.statements.GetSnapshotId.QueryRow().Scan(&oldSnapshotId); err != nil {
 		panic(err)
 	}
-	if newSnapshotId == snapshotId {
+	if newSnapshotId == oldSnapshotId {
 		fmt.Println("Snapshot IDs match, nothing to import")
 		return
 	}
 
 	// update snapshot_id
-	_, err = updateSnapshotId.Exec(newSnapshotId)
+	_, err = itx.statements.UpdateSnapshotId.Exec(newSnapshotId)
 	if err != nil {
 		fmt.Println("unable to update snapshot id")
 		panic(err)
 	}
 
-	// delete old songs. TRUNCATE not used as it doesn't support transactions
-	//
-	// NOTE:
-	// Spotify playlist max song is 10k -> worst case for deleting + inserting is few seconds
-	// refactor this if multiple playlists need to be tracked
-	_, err = deleteTracks.Exec()
-	if err != nil {
-		fmt.Println("unable to delete old tracks")
-		panic(err)
-	}
-
-	// insert new tracks into the database
+	// Add new tracks, update snapshot_id where applicable
+	// @TODO: implement batching
 	for nextPage != "" {
 		pg := fetchPageData(c, nextPage, token)
 		for _, e := range pg.Items {
 			date, _ := time.Parse(time.RFC3339, e.AddedAt)
-			_, err = insertTrack.Exec(
+			_, err := itx.statements.InsertUpdateTrack.Exec(
 				e.Track.ID,
 				e.Track.Name,
 				e.Track.Album.Name,
@@ -105,19 +91,27 @@ func main() {
 				e.AddedBy.ID,
 				date,
 				e.Track.DurationMs,
-				snapshotId,
+				newSnapshotId,
 			)
 			if err != nil {
 				fmt.Printf("Failed to insert new track: %s \n", err)
-				rollBackTx(tx)
+				rollBackTx(itx.tx)
 			}
 		}
+		fmt.Print(pg.Next)
 		nextPage = pg.Next
 	}
 
-	err = tx.Commit()
+	// delete old tracks
+	_, err = itx.statements.DeleteTracks.Exec(oldSnapshotId)
 	if err != nil {
-		rollBackTx(tx)
+		fmt.Printf("Failed to delete old tracks: %s \n", err)
+		rollBackTx(itx.tx)
+	}
+
+	err = itx.tx.Commit()
+	if err != nil {
+		rollBackTx(itx.tx)
 	}
 	fmt.Println("Done!")
 }
